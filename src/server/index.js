7c1b99bd6a492a17155f6aeb6285ebf40216708e
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-08-13 11:17:58 -0300

    refact: Use constant

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
+const port = 8080
 const app = express()
 
 app.use(express.static('dist'))
@@ -14,8 +14,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`)
 })
 
 app.get('/test', function (req, res) {
