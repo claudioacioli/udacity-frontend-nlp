7ced4f91c1f9a93437e37df17279c2be50ee24a7
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-08-13 19:52:40 -0300

    feat: Config parser

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,19 @@
 var path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
 const mockAPIResponse = require('./mockAPI.js')
-const port = 8080
+const port = 8081
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
