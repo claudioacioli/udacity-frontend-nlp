a188d6390e65eb0dd07a9815edb1fff6653412d2
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-08-14 14:28:19 -0300

    feat: Add utility

---
+++ src/client/js/api.js
@@ -0,0 +1,41 @@
+/**
+ * Wrapper to fetch Data
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * @Author: Claudio Acioli
+*/
+export const  
+  
+  getData = async (url, mode="cors") => {
+    const response = await fetch(url, {method:"GET", mode});
+    
+    try {
+      const result = await response.json();
+      return result;
+    } catch (ex) {
+      console.error("Error", ex);
+    }
+  },
+
+  postData = async (url, data={}, mode="cors") => {
+    const response = await fetch(url, {
+      method: "POST",
+      credentials: "same-origin",
+      body: JSON.stringify(data),
+      mode,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    try {
+      const result = await response.json();
+      return result;
+    } catch (ex) {
+      console.error("Error", ex);
+    }
+  }
+;
