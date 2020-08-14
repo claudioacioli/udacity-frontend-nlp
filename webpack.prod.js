aa8ee514e0d8616624c7d2df53d318fcbf1a25f2
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-08-13 22:13:31 -0300

    chore: Optimize production files

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -9,6 +11,13 @@ module.exports = {
     output: {
         libraryTarget: 'var',
         library: 'Client'
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({}), 
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     module: {
         rules: [
@@ -27,6 +36,9 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+            minify: {
+              collapseWhitespace: true
+            }
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css'
