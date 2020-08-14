15161f30cc6ec93f301c36505d9a3644001bb83d
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-08-14 14:22:21 -0300

    refact: Use library to write less code

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,15 +1,17 @@
+import { byId } from './by'
+
 function handleSubmit(event) {
   event.preventDefault()
 
   // check what text was put into the form field
-  let formText = document.getElementById('name').value
+  let formText = byId('name').value
   Client.checkForName(formText)
 
   console.log('::: Form Submitted :::')
   fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
-      document.getElementById('results').innerHTML = res.message
+      byId('results').innerHTML = res.message
   })
 }
 
