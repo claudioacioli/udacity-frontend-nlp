8e3f330c2e8e8e7a1033fe0adf89a44914c14588
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-08-15 13:49:00 -0300

    feat: Create test

---
+++ __test__/testNameChecker.spec.js
@@ -0,0 +1,5 @@
+import { checkForName } from '../src/client/js/nameChecker'
+
+describe('Testing the checker functionality', () => {
+  test('Testing the checkForName function', () => expect(checkForName).toBeDefined())
+})
