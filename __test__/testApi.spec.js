e5542bcd0d10b4ef07f264cce9945ae90ade70a6
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-08-15 14:55:49 -0300

    feat: Test async functions

--- __test__/testApi.spec.js
+++ __test__/testApi.spec.js
@@ -1,9 +1,34 @@
+import regeneratorRuntime from "regenerator-runtime";
 import { getData, postData } from '../src/client/js/api'
 
 describe('Testing Api utilitity', () => {
   
-  test('Testing the getData function', () => expect(getData).toBeDefined())
+  test('Testing the getData function', () => 
+    expect(getData).toBeDefined()
+  )
 
-  test('Testing the postData function', () => expect(postData).toBeDefined())
+  test('returns promise resolving the getData function', () => {
+    global.fetch = jest.fn(() => Promise.resolve({ json: () => ''}))
+    expect(getData('google.com')).resolves.toBe('')
+  })
+
+  test('returns promise rejects the getData function', () => {
+    global.fetch = jest.fn(() => Promise.reject(null))
+    expect(getData('google.com')).rejects.toBe(null)
+  })
+
+  test('Testing the postData function', () => 
+    expect(postData).toBeDefined()
+  )
+
+  test('returns promise resolving the postData function', () => {
+    global.fetch = jest.fn(() => Promise.resolve({ json: () => ''}))
+    expect(postData('google.com', {})).resolves.toBe('')
+  })
+
+  test('returns promise rejects the postData function', () => {
+    global.fetch = jest.fn(() => Promise.reject(null))
+    expect(postData('google.com', {})).rejects.toBe(null)
+  })
 
 })
